98056ec97ddf50455fe24319deba23fd8bf72e04
José Luís <joseluisramost3@gmail.com>
2025-01-21 20:46:52 -0300

    fix: arruma funcionamento do codigo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-
 import React, { useState } from 'react';
 
 function App() {
@@ -12,25 +11,32 @@ function App() {
       return;
     }
 
-    // Simulação de uma chamada gRPC
+    // Requisição para o backend
     const response = await fetch('http://localhost:8080/process', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ word })
     });
-    
 
+    // Verifica a resposta do backend
     const result = await response.json();
-    setResults([...results, result]);
+
+    // Atualiza o estado com os resultados
+    if (result.result) {
+      setResults(result.result);  // Atualiza corretamente com os dados recebidos
+    } else {
+      alert(result.error || 'Erro desconhecido.');
+    }
   };
 
   return (
     <div className="app-container">
       <div className="info-container">
         <h2>Como Utilizar</h2>
-        <p>Digite uma palavra no campo de entrada e clique no botão "Consultar Palavra". A tabela será atualizada com a palavra consultada e seu número de ocorrências.</p>
+        <p>Digite uma palavra no campo de entrada e clique no botão "Adicionar Palavra". A tabela será atualizada com a palavra adicionada e seu número de ocorrências.</p>
+        <p>Escreva "imprimir" e clique em "Adicionar Palavra" para ver a lista de palavras salvas e ocorrência.</p>
         <h2>Sobre o Projeto</h2>
         <p>Este projeto é um microserviço de dicionário de palavras utilizando gRPC e virtualização. Ele permite armazenar e contar palavras de forma distribuída.</p>
       </div>
@@ -42,7 +48,7 @@ function App() {
           onChange={(e) => setWord(e.target.value)}
           placeholder="Digite a palavra"
         />
-        <button onClick={consultWord}>Consultar Palavra</button>
+        <button onClick={consultWord}>Adicionar Palavra</button>
         <table>
           <thead>
             <tr>
@@ -54,7 +60,7 @@ function App() {
             {results.map((result, index) => (
               <tr key={index}>
                 <td>{result.word}</td>
-                <td>{result.sum}</td> {/* Atualizado para usar "sum" */}
+                <td>{result.sum}</td>  {/* Exibe o valor de "sum" */}
               </tr>
             ))}
           </tbody>
